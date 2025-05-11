a8b5937072fe478e0d862144b2947c71c55f934b
omar72osm <omar72osm@gmail.com>
2025-05-11 12:04:08 +0300

    last updetes
    route.js has been added to project and exported while nodeApp.js modified by exporting route.js file

--- nodeApp.js
+++ nodeApp.js
@@ -1,62 +1,8 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  // Set CORS Headers to Allow Requests from Other Ports
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow any domain
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS"); // Allow specific methods
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Allow JSON content
-
-  // Handle Preflight Requests (OPTIONS)
-  if (req.method === "OPTIONS") {
-    res.writeHead(204, { "Content-Type": "text/plain" });
-    res.end();
-    return;
-  }
-  let body = "";
-  const users = [];
-
-  if (req.url === "/" && req.method === "POST") {
-    console.log("url is :  / ");
-
-    req.on("data", (chunk) => {
-      body += chunk; // Collecting request data chunks
-    });
-    req.on("end", () => {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          status: "success",
-          message: "new user added is : ",
-          msgValue: "no one",
-        })
-      );
-    });
-  } else if (req.url === "/users" && req.method === "POST") {
-    // let body = "";
-
-    req.on("data", (chunk) => {
-      body += chunk; // Collecting request data chunks
-    });
-
-    req.on("end", () => {
-      const parsedData = JSON.parse(body); // Convert JSON string to an object
-      console.log("username is :", parsedData.username);
-      users.push(parsedData.username);
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          status: "success",
-          message: "new user added is ",
-          msgValue: parsedData.username,
-        })
-      );
-    });
-  } else {
-    res.writeHead(405, { "Content-Type": "text/plain" });
-    res.end("Only POST requests are accepted.");
-  }
-});
+const http = require('http');
+const routes = require('./route');
+// const server = http.createServer((req, res) => {});
+const server = http.createServer(routes);
 
 server.listen(3000, () =>
-  console.log("Server running on http://localhost:3000")
+  console.log('Server running on http://localhost:3000')
 );
---
+++ route.js
@@ -0,0 +1,57 @@
+const requestHandler = (req, res) => {
+  // Set CORS Headers to Allow Requests from Other Ports
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow any domain
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS'); // Allow specific methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow JSON content
+
+  // Handle Preflight Requests (OPTIONS)
+  if (req.method === 'OPTIONS') {
+    res.writeHead(204, { 'Content-Type': 'text/plain' });
+    res.end();
+    return;
+  }
+  let body = '';
+  const users = [];
+
+  if (req.url === '/' && req.method === 'POST') {
+    console.log('url is :  / ');
+
+    req.on('data', (chunk) => {
+      body += chunk; // Collecting request data chunks
+    });
+    req.on('end', () => {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(
+        JSON.stringify({
+          status: 'success',
+          message: 'new user added is : ',
+          msgValue: 'no one',
+        })
+      );
+    });
+  } else if (req.url === '/users' && req.method === 'POST') {
+    // let body = "";
+
+    req.on('data', (chunk) => {
+      body += chunk; // Collecting request data chunks
+    });
+
+    req.on('end', () => {
+      const parsedData = JSON.parse(body); // Convert JSON string to an object
+      console.log('username is :', parsedData.username);
+      users.push(parsedData.username);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(
+        JSON.stringify({
+          status: 'success',
+          message: 'new user added is ',
+          msgValue: parsedData.username,
+        })
+      );
+    });
+  } else {
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end('Only POST requests are accepted.');
+  }
+};
+module.exports = requestHandler;
