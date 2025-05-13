19dd17dc37c550fd4e11ed95a02a63e54504ec1d
omar72osm <omar72osm@gmail.com>
2025-05-13 07:48:29 +0300

    initialize package json file
    nodemon dependencies & "start": "nodemon nodeApp.js" script

--- nodeApp.js
+++ nodeApp.js
@@ -4,5 +4,5 @@ const routes = require('./route');
 const server = http.createServer(routes);
 
 server.listen(3000, () =>
-  console.log('Server running on http://localhost:3000')
+  console.log('Server is running on http://localhost:3000')
 );
